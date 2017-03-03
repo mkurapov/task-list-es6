bd73316490624f3d15367eab01a77e3cf39bf8b1
mkurapov <maxkurapov@gmail.com>
2017-03-03 14:55:00 -0700

    start of class

--- public/js/task-list.es6.js
+++ public/js/task-list.es6.js
@@ -1,1 +1,32 @@
-console.log('here doggies ');
+class TaskList {
+
+    constructor()
+    {
+        this.inputField= document.getElementById('task-input');
+        this.addButton = document.getElementById('add-task');
+        this.tasks = [];
+        this.addButton.addEventListener('click', () => this.addNewTask())
+    }
+
+    addNewTask()
+    {
+        const newTask = this.inputField.value;
+
+        if (newTask)
+        {
+            this.tasks.push(newTask)
+            this.inputField.value = '';
+        }
+
+
+    }
+
+
+
+
+}
+
+
+
+
+window.addEventListener('load', ()=> new TaskList())
